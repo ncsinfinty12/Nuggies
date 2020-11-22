61b19c9de45f784c9a07e46f135d1ae84d4e0680
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-11-22 19:43:12 +0530

    Update ban.js

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -12,7 +12,7 @@ module.exports.run = (client, message, args, utils) => {
 	User.ban({ reason: banReason });
 	const person = message.mentions.users.first();
 	const embed = new Discord.MessageEmbed()
-		.setTitle(person.username + ' got kicked by ' + message.author.username)
+		.setTitle(person.username + ' got banned by ' + message.author.username)
 		.setImage('https://media1.tenor.com/images/d8247f1369dd1ab6ebe9cb772ca12b96/tenor.gif?itemid=18150385');
 	message.channel.send(embed);
 };
@@ -20,9 +20,9 @@ module.exports.run = (client, message, args, utils) => {
 
 module.exports.help = {
 	aliases: [],
-	name: 'kick',
+	name: 'ban',
 	description: 'nein',
-	usage: 'kick',
+	usage: 'ban',
 };
 
 module.exports.config = {
