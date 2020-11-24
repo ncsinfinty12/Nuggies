f58460ac867d29c3d302610204f0985ff6875f45
TheRamann <69319475+TheRamann@users.noreply.github.com>
2020-11-24 08:58:57 +0530

    fixed clear command

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -2,9 +2,10 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args, utils) => {
+    if (!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("Invalid Permissions")
 	let number = args.join(' ');
 	number = parseInt(number);
-	message.channel.bulkDelete(number);
+	message.channel.bulkDelete(number + 1);
 	number = number.toString();
 	message.channel.send(number + ' messages have been deleted').then(m =>{
 		setTimeout(() =>{
