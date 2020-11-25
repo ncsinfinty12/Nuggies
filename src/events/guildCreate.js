575bee33c764cec4192ce030194922c6f31fbdce
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-11-25 21:09:12 +0530

    Update guildCreate.js

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,12 +1,5 @@
 exports.run = (guild) => {
-	let defaultChannel = '';
-	guild.channels.cache.forEach((channel) => {
-		if(channel.type == 'text' && defaultChannel == '') {
-			if(channel.permissionsFor(guild.me).has('SEND_MESSAGES')) {
-				defaultChannel = channel;
-			}
-		}
-		defaultChannel.send('thank you for inviting me !');
 
-	});
+	guild.channels.cache.first().send('thanks for inviting me to the server!');
+
 };
