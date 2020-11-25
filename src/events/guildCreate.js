3fa6a3e1e29892b160e28863ddb1fa74837c4d70
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-11-25 21:36:37 +0530

    Update guildCreate.js

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,5 +1,17 @@
 exports.run = async (guild) => {
 
-	await guild.channels.cache.first().send('thanks for inviting me to the server!');
+	let channelID;
+	const channels = guild.channels.cache;
 
+	channelLoop:
+	for (const key in channels) {
+		const c = channels[key];
+		if (c[1].type === 'text') {
+			channelID = c[0];
+			break channelLoop;
+		}
+	}
+
+	const channel = guild.channels.cache.get(guild.systemChannelID || channelID);
+	channel.send('Thanks for inviting me into this server!');
 };
