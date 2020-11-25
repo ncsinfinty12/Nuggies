5666ecef84c6904566a1a97267bbe096daa7c3a3
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-11-25 14:12:07 +0530

    Create eval.js

---
+++ src/commands/eval.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+function clean(text) {
+	if (typeof (text) === 'string') {return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));}
+	else {return text;}
+}
+
+module.exports.run = async (client, message, args, utils) => {
+	args = args.join(' ');
+	try {
+		let evaled = eval(args);
+		if (typeof evaled !== 'string') {evaled = require('util').inspect(evaled);}
+		message.channel.send(`\`\`\`xl\n${clean(evaled)}\n\`\`\``);
+	}
+	catch (err) {
+		message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+	}
+};
+
+module.exports.help = {
+	aliases: [],
+	name: 'eval',
+	description: 'nein',
+	usage: 'kick',
+};
+
+module.exports.config = {
+	args: true,
+	restricted: true,
+	category: 'misc',
+};
