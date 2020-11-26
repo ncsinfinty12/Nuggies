9c5ff72633cde2360b800c192a5e23a2e2ff8cb1
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-11-26 09:07:25 +0530

    Update hug.js

--- src/commands/hug.js
+++ src/commands/hug.js
@@ -3,14 +3,12 @@ const Discord = require('discord.js');
 const superagent = require('superagent');
 module.exports.run = async (client, message, args, utils) => {
 	if (!message.mentions.users.first()) return message.reply('You need to mention someone to hug them');
-	if(message.mentions.users.first().id === '734006373343297557') return message.reply('You can\'t hug him. He will explode on impact!');
-	if (message.mentions.users.first().id == client.user.id) return message.channel.send('Nuuuuuuuuuuuuuuuuuuuuuu that hugss');
 	const { body } = await superagent
 		.get('https://nekos.life/api/v2/img/hug');
 
 	const embed = new Discord.MessageEmbed()
 		.setColor('BLACK')
-		.setTitle(`${message.mentions.users.first().username}, you got huged by ${message.author.username}`)
+		.setTitle(`${message.mentions.users.first().username}, you got hugged by ${message.author.username}`)
 		.setImage(body.url)
 		.setFooter('that hugss');
 	message.channel.send({ embed });
