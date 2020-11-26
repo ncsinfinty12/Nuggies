450c3b59d0be7f6d527f563dc212415745a5b6b0
TheRamann <69319475+TheRamann@users.noreply.github.com>
2020-11-26 08:15:34 +0530

    say command

---
+++ src/commands/say.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+const Discord = require('discord.js');
+const config = require('../../utils/config.json');
+module.exports.run = async (client, message, args, utils) => {
+    const saymessage = args.join(" ");
+    message.delete().catch(err => console.log(err));
+    message.channel.send("**"+saymessage + "**\n\n -" + message.author.username)
+};
+
+
+module.exports.help = {
+	aliases: ['echo'],
+	name: 'say',
+	description: 'nein',
+	usage: 'say',
+};
+
+module.exports.config = {
+	args: false,
+	restricted: false,
+	category: 'misc',
+};
