7be9ba6c6f6226311cc63a5fc609c2ab87ef0977
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-11-26 08:37:59 +0530

    debugged

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -2,21 +2,24 @@
 const Discord = require('discord.js');
 const config = require('../../utils/config.json');
 module.exports.run = async (client, message, args, utils) => {
-    if(!message.author.bot){
-        if(!message.mentions.users.first()){
-            const embed = new discord.MessageEmbed()
-                .setColor(0x9f7ee0)
-                .setTitle(message.author.username + "'s Avatar")
-                .setImage(message.author.displayAvatarURL())
-            message.channel.send(embed);}
-        else{
-            const user = message.mentions.users.first()
-            const embed = new discord.MessageEmbed()
-                .setColor(0x9f7ee0)
-                .setTitle(user.username + "'s Avatar")
-                .setImage(user.displayAvatarURL())
-            message.channel.send(embed);
-}}}
+	if(!message.author.bot) {
+		if(!message.mentions.users.first()) {
+			const embed = new Discord.MessageEmbed()
+				.setColor(0x9f7ee0)
+				.setTitle(message.author.username + '\'s Avatar')
+				.setImage(message.author.displayAvatarURL());
+			message.channel.send(embed);
+		}
+		else{
+			const user = message.mentions.users.first();
+			const embed = new Discord.MessageEmbed()
+				.setColor(0x9f7ee0)
+				.setTitle(user.username + '\'s Avatar')
+				.setImage(user.displayAvatarURL());
+			message.channel.send(embed);
+		}
+	}
+};
 
 
 module.exports.help = {
