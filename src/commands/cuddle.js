aaa88521a5a0a9f7e25d5da534e06a76d5198423
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-11-26 15:39:50 +0530

    Create cuddle.js

---
+++ src/commands/cuddle.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+const Discord = require('discord.js');
+const superagent = require('superagent');
+module.exports.run = async (client, message, args, utils) => {
+	if (!message.mentions.users.first()) return message.reply('You need to mention someone to cuddle them');
+	if (message.mentions.users.first().id == client.user.id && message.author.id !== '242263403001937920') return message.channel.send('Aww! *cuddles you* ');
+	if (message.mentions.users.first().id == client.user.id && message.author.id == '242263403001937920') return message.reply('>///< *cuddles dev-san*');
+	const { body } = await superagent
+		.get('https://nekos.life/api/v2/img/cuddle');
+
+	const embed = new Discord.MessageEmbed()
+		.setColor('#ff9900')
+		.setTitle(`${message.author.username} cuddled ${message.mentions.users.first().username} OwO`)
+		.setImage(body.url)
+		.setFooter('cuddles uwu');
+	message.channel.send({ embed });
+};
+
+module.exports.help = {
+	aliases: [],
+	name: 'cuddle',
+	description: 'nein',
+	usage: 'kick',
+};
+
+module.exports.config = {
+	args: true,
+	restricted: false,
+	category: 'misc',
+};
