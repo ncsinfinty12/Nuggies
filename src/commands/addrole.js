da6623ccf036250801a1ae37da52d59f80a8b1eb
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-11-29 02:34:28 +0530

    Create addrole.js

---
+++ src/commands/addrole.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+const Discord = require('discord.js');
+const config = require('../../utils/config.json');
+module.exports.run = async (client, message, args, utils) => {
+	if (!message.guild.member(client.user).hasPermission('MANAGE_ROLES')) return message.reply('❌**Error:** I don\'t have the **Manage Roles** permission!');
+	if (message.mentions.users.size === 0) return message.reply('❌Please mention a user to give the role to.\nExample: `addrole @user Members`');
+	const member = message.guild.member(message.mentions.users.first());
+	if (!member) return message.reply('❌**Error:** That user does not seem valid.');
+	const rname = message.content.split(' ').splice(2).join(' ');
+	const role = message.guild.roles.cache.find(val => val.name === rname);
+	if (!role) return message.reply(`❌**Error:** ${rname} isn't a role on this server!`);
+	const botRolePosition = message.guild.member(client.user).roles.highest.position;
+	const rolePosition = role.position;
+	const userRolePossition = message.member.roles.highest.position;
+	if (userRolePossition <= rolePosition) return message.channel.send('❌**Error:** your role is lower than the specified role.');
+	if (botRolePosition <= rolePosition) return message.channel.send('❌**Error:**  my highest role is lower than the specified role.');
+	member.roles.add(role).catch(e => {
+		return message.channel.send(`❌**Error:**\n${e}`);
+	});
+	const be = new Discord.MessageEmbed()
+		.setTitle('Role Added!')
+		.setDescription(`**${message.author.username}**, I've added the **${rname}** role to **${message.mentions.users.first().username}**`);
+	message.channel.send(be);
+};
+
+module.exports.help = {
+	aliases: [],
+	name: 'addrole',
+	description: 'nein',
+	usage: 'addrole <@user> rolename',
+};
+
+module.exports.config = {
+	args: false,
+	restricted: false,
+	category: 'moderation',
+};
