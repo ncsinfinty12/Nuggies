92673dc360670e57b589e4b9d97c58ed690629e2
TheRamann <69319475+TheRamann@users.noreply.github.com>
2020-11-28 22:47:29 +0530

    added 8ball command

---
+++ src/commands/8ball.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-unused-vars */
+const Discord = require('discord.js');
+const config = require('../../utils/config.json');
+module.exports.run = async (client, message, args, utils) => {
+    const pronouns = ["silly","senpai","daddy","mommy","dad","mom","master","nii-san","onee-san","love","ma'am","sir","friend","b-baka","honey"];
+    const randompronoun = pronouns[Math.floor(Math.random() * (pronouns.length - 1 + 1) + 1)]
+    const question = args.join(" ");
+    if(!question) return message.channel.send(`${randompronoun}, ask a question 🥺`)
+    const answer = ["nu","yus","yes","no","never","of course","hell yeah","hell no","negative","positive","not today","only today","sadly yes","sadly no","maybe","you bet","not a chance","it's a secret","only for today"];
+    const randomanswer = answer[Math.floor(Math.random() * (answer.length - 1 + 1) + 1)]
+    const embed = new discord.MessageEmbed()
+        .setTitle(question)
+        .setDescription(`${randompronoun}, ${randomanswer}`)
+    message.channel.send(embed)
+};
+
+
+module.exports.help = {
+	aliases: [],
+	name: 'suggest',
+	description: 'nein',
+	usage: 'suggest',
+};
+
+module.exports.config = {
+	args: false,
+	restricted: false,
+	category: 'misc',
+};
