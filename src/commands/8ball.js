b6a731fa2cec68a69e2abd64b3e4ae981eec6a5b
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-11-29 02:18:39 +0530

    Update 8ball.js

--- src/commands/8ball.js
+++ src/commands/8ball.js
@@ -2,16 +2,16 @@
 const Discord = require('discord.js');
 const config = require('../../utils/config.json');
 module.exports.run = async (client, message, args, utils) => {
-    const pronouns = ["silly","senpai","daddy","mommy","dad","mom","master","nii-san","onee-san","love","ma'am","sir","friend","b-baka","honey"];
-    const randompronoun = pronouns[Math.floor(Math.random() * (pronouns.length - 1 + 1) + 1)]
-    const question = args.join(" ");
-    if(!question) return message.channel.send(`${randompronoun}, ask a question 🥺`)
-    const answer = ["nu","yus","yes","no","never","of course","hell yeah","hell no","negative","positive","not today","only today","sadly yes","sadly no","maybe","you bet","not a chance","it's a secret","only for today"];
-    const randomanswer = answer[Math.floor(Math.random() * (answer.length - 1 + 1) + 1)]
-    const embed = new Discord.MessageEmbed()
-        .setTitle(question)
-        .setDescription(`${randompronoun}, ${randomanswer}`)
-    message.channel.send(embed)
+	const pronouns = ['silly', 'senpai', 'daddy', 'mommy', 'dad', 'mom', 'master', 'nii-san', 'onee-san', 'love', 'ma\'am', 'sir', 'friend', 'b-baka', 'honey'];
+	const randompronoun = pronouns[Math.floor(Math.random() * (pronouns.length - 1 + 1) + 1)];
+	const question = args.join(' ');
+	if(!question) return message.channel.send(`${randompronoun}, ask a question 🥺`);
+	const answer = ['nu', 'yus', 'yes', 'no', 'never', 'of course', 'hell yeah', 'hell no', 'negative', 'positive', 'not today', 'only today', 'sadly yes', 'sadly no', 'maybe', 'you bet', 'not a chance', 'it\'s a secret', 'only for today'];
+	const randomanswer = answer[Math.floor(Math.random() * (answer.length - 1 + 1) + 1)];
+	const embed = new Discord.MessageEmbed()
+		.setTitle(question)
+		.setDescription(`${randompronoun}, ${randomanswer}`);
+	message.channel.send(embed);
 };
 
 
