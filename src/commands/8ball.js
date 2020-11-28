1a3276bbba6b060e88fafee21ec603c8be31c756
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-11-29 02:22:15 +0530

    alias

--- src/commands/8ball.js
+++ src/commands/8ball.js
@@ -16,7 +16,7 @@ module.exports.run = async (client, message, args, utils) => {
 
 
 module.exports.help = {
-	aliases: [],
+	aliases: ['ball'],
 	name: '8ball',
 	description: 'nein',
 	usage: '8ball',
