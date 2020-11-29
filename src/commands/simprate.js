5fc22b9dd228dc27e3e3746525185737b493d028
TheRamann <69319475+TheRamann@users.noreply.github.com>
2020-11-29 12:19:24 +0530

    simprate �

---
+++ src/commands/simprate.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+const Discord = require('discord.js');
+
+module.exports.run = async (client, message, args, utils) => {
+	var user;
+	if(!message.mentions.users.first()){user = message.author}
+	else{ user = message.mentions.users.first()}
+	const rate = Math.floor(Math.random() * (100 - 1 + 1) + 1)
+	const embed = new Discord.MessageEmbed()
+		.setTitle(`${user.username}'s simprate`)
+		.setDescription(`${user.username} is ${rate}% simp`)
+	message.channel.send(embed)
+};
+
+
+module.exports.help = {
+	aliases: [],
+	name: 'simprate',
+	description: 'nein',
+	usage: 'simprate',
+};
+
+module.exports.config = {
+	args: false,
+	restricted: false,
+	category: 'misc',
+};
