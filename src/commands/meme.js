9fb3be828181a6ed8311d92aab302c86fb770044
TheRamann <69319475+TheRamann@users.noreply.github.com>
2020-12-01 14:38:39 +0530

    meme command

---
+++ src/commands/meme.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+const Discord = require('discord.js');
+const superagent = require('superagent');
+
+module.exports.run = async (client, message, args, utils) => {
+
+	(async () => {
+		const body = await superagent('https://meme-api.herokuapp.com/gimme');
+		const embed = new Discord.MessageEmbed()
+			.setColor(0xab4aff)
+			.setTitle(body.body.title)
+			.setImage(body.body.url)
+			.setURL(body.body.postLink);
+		message.channel.send(embed);
+	})();
+};
+
+
+module.exports.help = {
+	aliases: [],
+	name: 'ping',
+	description: 'nein',
+	usage: 'ping',
+};
+
+module.exports.config = {
+	args: false,
+	restricted: false,
+	category: 'misc',
+};
