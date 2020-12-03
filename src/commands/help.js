4e8d77aaf958112e7eb660417b52cac3b57c7787
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-12-03 10:16:34 +0530

    Update help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -6,15 +6,35 @@ module.exports.run = async (client, message, args, utils) => {
 	if(!args[0]) {
 		const b = new Discord.MessageEmbed()
 			.setTitle('Command list for Nuggies:')
-			.addField('For more info about a specific Command:', 'Use [prefix]help command_name')
-			.addField('Moderation', help.mod)
-			.addField('Fun', help.fun)
-			.addField('Information', help.info)
-			.addField('misc', help.misc)
-			.addField('Owner', help.owner)
+			.setDescription('For more info about a specific Command: \n Use [prefix]help command_name')
+			.addField('Moderation', help.mod, true)
+			.addField('Fun', help.fun, true)
+			.addField('Actions', help.action, true)
+			.addField('Information', help.info, true)
+			.addField('Music', '*coming soon ..*')
+			.addField('misc', help.misc, true)
+			.addField('Owner', help.owner, true)
 			.setFooter('use "." before every command !')
+			.setThumbnail(client.user.displayAvatarURL())
+			.setImage('https://media.discordapp.net/attachments/746028038021709965/783744416119783455/x.png')
 			.setColor(Math.floor(Math.random() * 16777215));
 		message.channel.send(b);
+	}
+	else{
+		const command = args[0];
+		if (client.commands.has(command)) {
+			const cmd = client.commands.get(command);
+			const b = new Discord.MessageEmbed()
+				.setTitle('commmand info:')
+				.addField('``Description:``', cmd.help.description, true)
+				.addField('``Usage:``', cmd.help.usage, true);
+			message.channel.send(b);
+			return;
+
+		}
+		else {
+			return message.reply('That command doesn\'t exist!');
+		}
 	}
 };
 
