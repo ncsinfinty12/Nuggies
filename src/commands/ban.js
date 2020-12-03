62db7aca9ee896973d53db4c2aac2a4a81a30824
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-12-03 21:46:00 +0530

    Update ban.js

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -28,7 +28,7 @@ module.exports.run = async (client, message, args, utils) => {
 			.addField('Reason', reason)
 			.setThumbnail('https://media1.tenor.com/images/6a61251f5453d93d76f9826be10b7f49/tenor.gif?itemid=7580925');
 		// let obj = JSON.parse(`{"days":7, "reason": ${reason}}`)
-		if(user.bot) return;
+		if(user.bot) return message.channel.send('the user is a bot ! I cant do that to my own race :pensive:');
 		message.mentions.users.first().send({ embed }).catch(e =>{
 			console.log(e);
 		});
