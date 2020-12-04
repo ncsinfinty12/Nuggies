1eb39db42f231dca20c02cdf8c98b8705f082a3d
TheRamann <69319475+TheRamann@users.noreply.github.com>
2020-12-04 12:45:42 +0530

    imdb command :heart:

---
+++ src/commands/imdb.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-unused-vars */
+const Discord = require('discord.js');
+const config = require('../../utils/config.json');
+const movie = require('node-movie');
+
+module.exports.run = async (client, message, args, utils) => {
+	const saymessage = args.join(' ');
+	movie(saymessage, data => {
+		const embed = new Discord.MessageEmbed()
+			.setDescription('**Date released:** ' + data.Released + '\n**Runtime**: ' + data.Runtime + '\n**Genre:** ' + data.Genre + '\n**Plot:** ' + data.Plot + '\n**Actors:** ' + data.Actors + '\n**Awards Received:**: ' + data.Awards)
+			.setThumbnail(data.Poster)
+			.setTitle(data.Title);
+		message.channel.send(embed);
+	});
+};
+
+
+module.exports.help = {
+	aliases: ['movie', 'show'],
+	name: 'imdb',
+	description: 'Want information about movies? We got you covered',
+	usage: config.prefix + 'imdb Avengers Endgame',
+};
+
+module.exports.config = {
+	args: false,
+	restricted: false,
+	category: 'misc',
+};
