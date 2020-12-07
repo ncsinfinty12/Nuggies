b9dc6886b6bdfb10df67744eb2ce4a700c10e04c
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-12-07 11:20:12 +0530

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,5 +2,5 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 exports.run = () => {
 	console.log('bot is online.');
-	client.user.setActivity(`watching ${client.guilds.cache.size} servers !`, { type: 'COMPETING' });
+	client.user.setActivity(`watching ${client.guilds.cache.size} servers !`, { type: 'COMPETING', url: 'https://discord.gg/zzURhQGpRY' });
 };
