1e810b3c3ce9a6a5021f6a0c9b0defec1647d0ea
TheRamann <69319475+TheRamann@users.noreply.github.com>
2020-12-08 20:38:45 +0530

    that's a fact

---
+++ src/commands/fact.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars */
+const Discord = require('discord.js');
+const superagent = require('superagent');
+const config = require('../../utils/config.json');
+
+module.exports.run = async (client, message, args, utils) => {
+	if (message.mentions.users.first().id === client.user.id) return message.channel.send('<a:yayyy:497742636439044096>');
+	const { body } = await superagent
+		.get('https://nekos.life/api/v2/img/fact');
+
+	const embed = new Discord.MessageEmbed()
+		.setColor('BLACK')
+		.setTitle('And That\'s a Fact')
+		.setDescription(body.fact)
+		.setFooter('fact fact fact fact fact fact fact fact fact fact fact fact fact fact fact fact fact fact');
+	message.channel.send({ embed });
+};
+
+
+module.exports.help = {
+	aliases: [],
+	name: 'fact',
+	description: 'just facts',
+	usage: config.prefix + 'fact',
+};
+
+module.exports.config = {
+	args: false,
+	restricted: false,
+	category: 'misc',
+};
