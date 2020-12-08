62cbaeab1a20e1245d8757e52bfdb7a5df17a1c5
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-12-08 20:24:50 +0530

    added topic command

---
+++ src/topic.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+const config = require('../../utils/config.json');
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+module.exports.run = async (client, message, args, utils) => {
+	rp('https://www.conversationstarters.com/generator.php').then(html => {
+		const $ = cheerio.load(html);
+		const text = $('#random').text();
+		text
+			? message.channel.send(` > ${text}`)
+			: message.channel.send('Wooops! Looks like something happened while trying to retrieve a topic.');
+	});
+};
+
+module.exports.help = {
+	aliases: [],
+	name: 'topic',
+	description: 'get a topic to start a conversation !',
+	usage: '.topic',
+};
+
+module.exports.config = {
+	args: true,
+	restricted: false,
+	category: 'moderation',
+};
