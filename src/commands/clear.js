1f506310b1a323e5f0d884178900c872313a3673
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-12-09 19:55:58 +0530

    fixed bug

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -6,12 +6,11 @@ module.exports.run = async (client, message, args, utils) => {
 	if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Invalid Permissions');
 	let number = args.join(' ');
 	number = parseInt(number);
-	if(number > 100) {
-		message.reply('Please provide a number smaller than 100 !').then(m =>{
+	if(number > 99) {
+		return message.reply('Please provide a number smaller than 100 !').then(m =>{
 			setTimeout(() =>{
 				m.delete();
 			}, 5000);
-			return;
 		});
 	}
 	message.channel.bulkDelete(number + 1);
