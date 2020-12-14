27ed960a82ac97af3d7ee309e8d155aba894ff24
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-12-14 09:49:34 +0530

    bug

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -7,6 +7,7 @@ module.exports.run = async (client, message, args, utils) => {
 	let number = args.join(' ');
 	if(isNaN(number)) return message.channel.send('That is not a number');
 	number = parseInt(number);
+	if(number < 1) return message.reply('please provide a number larger than 1');
 	if(number > 99) {
 		return message.reply('Please provide a number smaller than 100 !').then(m =>{
 			setTimeout(() =>{
