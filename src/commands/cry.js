8630302dc7ad07855adef99e2a7bf188699ca438
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-12-25 23:52:16 +0530

    final

---
+++ src/commands/cry.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+const Discord = require('discord.js');
+const superagent = require('superagent');
+const config = require('../../utils/config.json');
+
+module.exports.run = async (client, message, args, utils) => {
+	const { body } = await superagent
+		.get('http://api.nekos.fun:8080/api/cry');
+	const m = new Discord.MessageEmbed()
+		.setTitle(`${message.author.username} is crying ! 😦`)
+		.setImage(body.image)
+		.setFooter('😭')
+		.setColor('BLACK');
+	message.channel.send(m);
+
+};
+
+module.exports.help = {
+	aliases: [],
+	name: 'cry',
+	description: 'cry 😭😭😭',
+	usage: config.prefix + 'cry',
+};
+
+module.exports.config = {
+	args: false,
+	restricted: false,
+	category: 'misc',
+	disable: false,
+};
