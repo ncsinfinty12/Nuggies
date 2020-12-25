c18ad34578ddc1ec29b30cfcecc4c2fb0a993da0
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-12-25 19:59:22 +0530

    Create laugh.js

---
+++ src/commands/laugh.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+const Discord = require('discord.js');
+const superagent = require('superagent');
+const config = require('../../utils/config.json');
+
+module.exports.run = async (client, message, args, utils) => {
+	const { body } = await superagent
+		.get('http://api.nekos.fun:8080/api/laugh');
+	const m = new Discord.MessageEmbed()
+		.setTitle(`${message.author.username} is laughing !`)
+		.setImage(body.image)
+		.setFooter('HaHaHaHa')
+		.setColor('BLACK');
+	message.channel.send(m);
+
+};
+
+module.exports.help = {
+	aliases: [],
+	name: 'laugh',
+	description: 'laugh 😂',
+	usage: config.prefix + 'laugh',
+};
+
+module.exports.config = {
+	args: false,
+	restricted: false,
+	category: 'misc',
+	disable: false,
+};
