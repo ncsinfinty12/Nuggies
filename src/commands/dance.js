a6993b20b1e0ffbfd8760fcec1c6c38d94adf0f0
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2021-01-03 00:15:40 +0530

    dance.js

--- src/commands/dance.js
+++ src/commands/dance.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const axios = require('axios');
 const config = require('../../utils/config.json');
 module.exports.run = async (client, message, args, utils) => {
-	axios.get('https://api.otakugifs.xyz/gif/blush', {
+	axios.get('https://api.otakugifs.xyz/gif/dance', {
 		headers: {
 			'X-API-KEY': 'pLsoTHg2vHBKpB4CNeGnVysCP60645uW8fFRRbgT7AIvkyHbBgE3IsgNBS3rUuD8321l23GHAT8GfbE4K4c9T0qH9P2',
 		},
@@ -11,8 +11,8 @@ module.exports.run = async (client, message, args, utils) => {
 		.then(function(response) {
 			const gifurl = response.data.url;
 			const embed = new Discord.MessageEmbed()
-				.setTitle(`${message.author.username} is blushing ! 😊`)
-				.setFooter('blushy blushy')
+				.setTitle(`${message.author.username} is dancing ! 💃🕺💃🕺`)
+				.setFooter('move 😍 that 😀 sexy 🤪 body 🥰')
 				.setColor('RANDOM')
 				.setImage(gifurl);
 			message.channel.send(embed);
@@ -24,9 +24,9 @@ module.exports.run = async (client, message, args, utils) => {
 
 module.exports.help = {
 	aliases: [],
-	name: 'blush',
-	description: 'blush !',
-	usage: config.prefix + 'blush',
+	name: 'dance',
+	description: 'dance !',
+	usage: config.prefix + 'dance',
 };
 
 module.exports.config = {
