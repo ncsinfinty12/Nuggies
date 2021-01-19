8a466bb4c3723d442ec34c7db599d8fa31637b7d
asdf qwerty <chordsandfrets@gmail.com>
2021-01-19 10:55:35 +0530

    finished sharding **mic drop**

--- shard.js
+++ shard.js
@@ -1,11 +1,11 @@
 const { ShardingManager } = require('discord.js');
 
 const manager = new ShardingManager('./index.js', {
-	totalShards: 4,
-	token: 'NzQxMDAwODY1Mjg4MjkwNDM1.XyxM1Q.9l4FuhpAyjzoT7zZrjnNzreb-lk',
+	totalShards: 2,
+	token: 'Nzc5NzQxMTYyNDY1NTI1Nzkw.X7k8jA.qKeVTZ74GyIut_Hb8kZVGGrp4TM',
+});
 	// token for beta - NzQxMDAwODY1Mjg4MjkwNDM1.XyxM1Q.9l4FuhpAyjzoT7zZrjnNzreb-lk
 	// token for nuggies - Nzc5NzQxMTYyNDY1NTI1Nzkw.X7k8jA.qKeVTZ74GyIut_Hb8kZVGGrp4TM
-});
 
 manager.spawn();
 
--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -4,7 +4,7 @@ const config = require('../../../utils/config.json');
 
 module.exports.run = async (client, message, args, utils) => {
 	const start = message.createdTimestamp;
-
+	const users = await client.shard.fetchClientValues('users.cache.size');
 	message.channel.send({ embed: { description: 'pinging...', color: 0x00FFFF } }).then(m => {
 
 		const end = Date.now();
@@ -16,7 +16,7 @@ module.exports.run = async (client, message, args, utils) => {
 				{ name: 'Message Latency', value: end - start + 'ms', inline: true },
 				{ name: '\u200B', value: '\u200B', inline: true },
 				{ name: 'Commands Loaded', value: client.commands.size, inline: true },
-				{ name: 'User\'s Cached', value: client.users.cache.size, inline: true },
+				{ name: 'Users Cached', value: users.reduce((acc, count) => acc + count, 0), inline: true },
 			)
 			.setColor('RANDOM');
 		m.edit(embed).catch(e => message.channel.send(e));
