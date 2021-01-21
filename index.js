7a28e3c231e72c27d60074256339469d74ad87f0
asdf qwerty <chordsandfrets@gmail.com>
2021-01-21 13:25:38 +0530

    token

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ async function startUp() {
 			data.save();
 		});
 	};
-	client.login('NzQxMDAwODY1Mjg4MjkwNDM1.XyxM1Q.9l4FuhpAyjzoT7zZrjnNzreb-lk');
+	client.login('Nzc5NzQxMTYyNDY1NTI1Nzkw.X7k8jA.qKeVTZ74GyIut_Hb8kZVGGrp4TM');
 }
 
 startUp();
