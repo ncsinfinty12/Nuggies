7fcc6efb5c087bb876e4f9b0169fabeb26bfe67e
pro_gamer007 <coolaryan@icloud.com>
2021-02-12 12:56:15 +0530

    doesnt work but its going somewhere?

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ process.on('unhandledRejection', async (err) => {
 
 startUp();
 
-client.login('Nzc5NzQxMTYyNDY1NTI1Nzkw.X7k8jA.qNuxbT2n0ce8FnMMCdmmP-VjcRU');
+client.login('NzQxMDAwODY1Mjg4MjkwNDM1.XyxM1Q.9l4FuhpAyjzoT7zZrjnNzreb-lk');
 // token for beta - NzQxMDAwODY1Mjg4MjkwNDM1.XyxM1Q.9l4FuhpAyjzoT7zZrjnNzreb-lk
 // token for nuggies - Nzc5NzQxMTYyNDY1NTI1Nzkw.X7k8jA.qNuxbT2n0ce8FnMMCdmmP-VjcRU
 
