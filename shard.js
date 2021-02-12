5e211a3bdd53259ae28d99921e7a9715ba28e680
pro_gamer007 <coolaryan@icloud.com>
2021-02-12 08:51:51 +0530

    meme cmd using our api and easier value to shard id?

--- shard.js
+++ shard.js
@@ -6,6 +6,6 @@ const manager = new ShardingManager('./index.js', {
 // token for beta - NzQxMDAwODY1Mjg4MjkwNDM1.XyxM1Q.cKxvxEcyPI3HCd9-jcqVYgghgGs
 // token for nuggies - Nzc5NzQxMTYyNDY1NTI1Nzkw.X7k8jA.4KGlhAqzYXkTDDADuP19-JRo1qc
 
-manager.on('shardCreate', (shard) => console.log(`Shard ${shard.id} launched`));
+manager.on('shardCreate', (shard) => console.log(`Shard ${shard.id + 1} launched`));
 
 manager.spawn(2, 15000, -1);
