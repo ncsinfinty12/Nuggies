94c4ae734c272d9ee7c7aca465d10b0051e58061
pro_gamer007 <66844505+pro-gamer007@users.noreply.github.com>
2021-02-12 10:22:38 +0530

    token

--- shard.js
+++ shard.js
@@ -1,10 +1,10 @@
 const { ShardingManager } = require('discord.js');
 
 const manager = new ShardingManager('./index.js', {
-	token: 'Nzc5NzQxMTYyNDY1NTI1Nzkw.X7k8jA.4KGlhAqzYXkTDDADuP19-JRo1qc',
+	token: 'Nzc5NzQxMTYyNDY1NTI1Nzkw.X7k8jA.qNuxbT2n0ce8FnMMCdmmP-VjcRU',
 });
 // token for beta - NzQxMDAwODY1Mjg4MjkwNDM1.XyxM1Q.cKxvxEcyPI3HCd9-jcqVYgghgGs
-// token for nuggies - Nzc5NzQxMTYyNDY1NTI1Nzkw.X7k8jA.4KGlhAqzYXkTDDADuP19-JRo1qc
+// token for nuggies - Nzc5NzQxMTYyNDY1NTI1Nzkw.X7k8jA.qNuxbT2n0ce8FnMMCdmmP-VjcRU
 
 manager.on('shardCreate', (shard) => console.log(`Shard ${shard.id} launched`));
 
