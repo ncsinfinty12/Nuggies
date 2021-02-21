2fb9923573f6ea4cf52b4c20dad75837afa4ec31
Assassin-1234 <chordsandfrets@gmail.com>
2021-02-21 17:12:11 +0530

    so many new commands holy shit

---
+++ src/commands/action/peek.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-unused-vars */
+const Discord = require('discord.js');
+const axios = require('axios');
+const config = require('../../../utils/config.json');
+module.exports.run = async (client, message, args, utils) => {
+	if (!message.mentions.users.first()) return message.reply('You need to mention someone to peek!');
+	if (message.mentions.users.first().id == message.author.id) return message.reply('I dont think you can');
+	axios.get('https://api.otakugifs.xyz/gif/peek', {
+		headers: {
+			'X-API-KEY': 'FrDrFYi5XFr7dijUh6uhMcTnc0r5MeAIrHp2waZo8FWrk3LuRzvYgbmrVW4Z04pFgzcXf2x5yxJUJ7DDslmDqb',
+		},
+	})
+		.then(function(response) {
+			const gifurl = response.data.url;
+			const embed = new Discord.MessageEmbed()
+				.setTitle(`${message.mentions.users.first().username} ! ${message.author.username} is peeking at you!`)
+				.setFooter('👁👁')
+				.setColor('RANDOM')
+				.setImage(gifurl);
+			message.channel.send(embed);
+		})
+		.catch(function(error) {
+			message.channel.send('❌**Error:** ' + error);
+		});
+};
+
+module.exports.help = {
+	aliases: [],
+	name: 'peek',
+	description: 'peek at someone !',
+	usage: config.prefix + 'love',
+};
+
+module.exports.config = {
+	args: false,
+	restricted: false,
+	category: 'Actions',
+	disable: false,
+	cooldown: 1000,
+};
