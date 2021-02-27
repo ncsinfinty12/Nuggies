0daa7ad42319572fba3b224ece184d8dfe18c4c6
Assassin-1234 <chordsandfrets@gmail.com>
2021-02-27 23:36:10 +0530

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,5 +5,5 @@ const Discord = require('discord.js');
 module.exports = async (client) => {
 
 	console.log(`${client.user.username} is now online!`);
-	client.user.setActivity(`to ${client.guilds.cache.size} guilds!`, { type: 'STREAMING', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' });
+	client.user.setActivity(`to ${client.guilds.cache.size} servers!`, { type: 'STREAMING', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' });
 };
