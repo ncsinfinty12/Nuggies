fb4a631d1418239e436bd71f678e9b419fe660f9
birb <62964708+Gav-King@users.noreply.github.com>
2021-03-09 09:16:40 -0500

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Discord = require('discord.js');
 const automeme = require('../../models/guilds');
 module.exports = async (client) => {
-
+	client.manager.init(client.user.id);
 	console.log(`${client.user.username} is now online!`);
 	client.user.setActivity(`to ${client.guilds.cache.size} servers!`, { type: 'STREAMING', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' });
 	automeme.find({}, { automeme_channel:1, _id:0 }, async (err, data) => {
