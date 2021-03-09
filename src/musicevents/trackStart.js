c6f7b8e34b780c461ec069f83df57ff328bed569
birb <62964708+Gav-King@users.noreply.github.com>
2021-03-09 10:36:04 -0500

    trackStart event

--- src/musicevents/trackStart.js
+++ src/musicevents/trackStart.js
@@ -1,1 +1,11 @@
-// nothing for now teehee
+const Discord = require('discord.js');
+
+module.exports = (client, player, track) => {
+    const channel = client.channels.cache.get(player.textChannel);
+    
+    const embed = new Discord.MessageEmbed()
+    .setTitle('New Track')
+    .setDescription(`Now Playing __**[${track.title}](${track.uri})**__ requested by ${track.requester.tag}`)
+    .setColor('BLUE');
+    channel.send(embed);
+}
