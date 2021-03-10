97c873522f9b11ab7f2c8d487a640dae1f1b853a
birb <62964708+Gav-King@users.noreply.github.com>
2021-03-10 08:56:12 -0500

    me when think fix

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const unhhook = new Discord.WebhookClient(config.unhhookID, config.unhhookTOKEN)
 async function startUp() {
 
 	// load music events
-	const musicEvents = fs.readdirSync('./src/musicevents').filter(file => file.endsWith('.js'));
+	const musicEvents = fs.readdirSync('./src/musicevents/').filter(file => file.endsWith('.js'));
 	console.log(`Loading a total of ${musicEvents.length} events.`);
 	for (const file of musicEvents) {
 		const musicEvent = require(`./src/musicevents/${file}`);
