7c79704a0dd9e1bc3747b5d93458cc241efe807f
Assassin-1234 <chordsandfrets@gmail.com>
2021-03-15 00:10:36 +0530

    setuserpremium fixed + deleted updatecount.js

--- src/commands/owner/setuserpremium.js
+++ src/commands/owner/setuserpremium.js
@@ -1,21 +1,19 @@
 /* eslint-disable no-unused-vars */
-const { premiumUser, getUserDB } = require('../../../functions/mongo');
-const config = require('../../../utils/config.json');
 const Discord = require('discord.js');
 const users = require('../../../models/users');
 module.exports.run = async (client, message, args, utils, data) => {
-	if(!args[0]) return message.channel.send(new Discord.MessageEmbed().setTitle('Error').setColor('RED').setDescription('Please provide a user.'));
-	const target = message.mentions.users.first() || client.users.cache.get(args[0]);
-	if(!target) return message.channel.send('user doesnt exist');
-	if(!args[1]) return message.channel.send(new Discord.MessageEmbed().setTitle('Error').setColor('RED').setDescription('Please provide a toggle.'));
+	const target = args[0];
 	const toggle = args[1];
-	try {
-		premiumUser(target.id, toggle);
-		return message.channel.send(new Discord.MessageEmbed().setTitle('Success!').setDescription(`premium set to \`${toggle}\` for **${target.tag}**! \n \n`).setFooter('thanks for being a donor :)').setColor('GREEN'));
-	}
-	catch (e) {
-		message.channel.send(e);
-	}
+	if(!target) return message.channel.send(new Discord.MessageEmbed().setTitle('Error').setDescription('Please provide a user!').setColor('RED'));
+	if(!toggle) return message.channel.send(new Discord.MessageEmbed().setTitle('Error').setDescription('Please provide a toggle!').setColor('RED'));
+	// if(toggle != 'true' || 'false') return message.channel.send(new Discord.MessageEmbed().setTitle('Error').setDescription('Please provide a valid toggle!'));
+	users.findOne({ id: args[0] }, async (err, Data) => {
+		if(err) throw err;
+		if(!Data) return message.channel.send(new Discord.MessageEmbed().setTitle('Error').setDescription('user not found').setColor('RED'));
+		Data.premium = toggle;
+		Data.save();
+		message.channel.send(new Discord.MessageEmbed().setTitle('Success!').setDescription(`premium set to \`${toggle.toLowerCase()}\` for **${client.users.cache.get(target).tag}** `));
+	});
 };
 module.exports.help = {
 	aliases: ['setuserp', 'userp'],
