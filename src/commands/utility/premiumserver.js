72e862ac7831847a9331661bdfe7426296c309f5
Assassin-1234 <chordsandfrets@gmail.com>
2021-03-27 14:46:35 +0530

    added tier limitations

--- src/commands/utility/premiumserver.js
+++ src/commands/utility/premiumserver.js
@@ -7,14 +7,10 @@ const config = require('../../../utils/config.json');
 module.exports.run = async (client, message, args, utils, data) => {
 	// check if the server is already premium or not
 	if(data.user.premium == true) return message.channel.send(new Discord.MessageEmbed().setTitle('Error').setDescription('This server is already premium!').setColor('RED'));
-	// dev check
-	if(config.developers.includes(message.author.id)) {
-		premiumGuild(message.guild.id, true);
-		return message.channel.send(new Discord.MessageEmbed().setTitle('Success!').setDescription(`premium set to \`true\` in ${message.guild.name}! \n \n`).setFooter('thanks for being a donor :)').setColor('GREEN'));
-	}
 	// premium user check
 	if(data.user.premium == false) return message.channel.send(new Discord.MessageEmbed().setTitle('Error').setDescription('This command is only for donors, please [__**click here**__](https://goo.gl/CWqeBF) to donate!').setColor('RED'));
 	// changing boolean value
+	if(data.user.tier == 1 && data.user.premiumservers.length == 1 || data.user.tier == 2 && data.user.premiumservers.length == 3 || data.user.tier == 3 && data.user.premiumservers.length == 5 || data.user.tier == 4 && data.premiumservers.length == 2) return message.channel.send(new Discord.MessageEmbed().setTitle('Error').setDescription('You cannot add premium to any guilds. Please remove premium from a guild to add it!').setColor('RED'));
 	if(data.user.premium == true) {
 		premiumGuild(message.guild.id, true);
 		message.channel.send(new Discord.MessageEmbed().setTitle('Success!').setDescription(`premium set to \`true\` in ${message.guild.name}! \n \n`).setFooter('thanks for being a donor :)').setColor('GREEN'));
