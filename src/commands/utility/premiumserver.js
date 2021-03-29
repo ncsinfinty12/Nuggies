19541954f49661c7cafb970d2c36efe265f9ea91
Assassin-1234 <chordsandfrets@gmail.com>
2021-03-29 21:46:06 +0530

    finished premiumserveradd command!

--- src/commands/utility/premiumserver.js
+++ src/commands/utility/premiumserver.js
@@ -5,29 +5,26 @@ const guildDB = require('../../../models/guilds');
 const { premiumGuild, pushguild } = require('../../../functions/mongo');
 const config = require('../../../utils/config.json');
 module.exports.run = async (client, message, args, utils, data) => {
-	// check if the server is already premium or not
-	if(data.guild.premium == true) return message.channel.send(new Discord.MessageEmbed().setTitle('Error').setDescription('This server is already premium!').setColor('RED'));
-	// premium user check
 	if(data.user.premium == false) return message.channel.send(new Discord.MessageEmbed().setTitle('Error').setDescription('This command is only for donors, please [__**click here**__](https://goo.gl/CWqeBF) to donate!').setColor('RED'));
-
+	// check if the server is already premium or not
 	if(data.guild.premium == true) return message.channel.send(new Discord.MessageEmbed().setTitle('Error').setDescription('This server is already premium!').setColor('RED'));
 	if(data.user.tier == 1 && data.user.premiumservers.length == 1 || data.user.tier == 2 && data.user.premiumservers.length == 3 || data.user.tier == 3 && data.user.premiumservers.length == 5 || data.user.tier == 4 && data.premiumservers.length == 2) return message.channel.send(new Discord.MessageEmbed().setTitle('Error').setDescription('You cannot add premium to any guilds. Please remove premium from a guild to add it!').setColor('RED'));
 	// making the guild premium.
 	try {
-		premiumGuild(message.guild.id, true);
-		pushguild(message.author.id, message.guild.id);
+		await premiumGuild(message.guild.id, 'true');
+		await pushguild(message.author.id, message.guild.id, 'push');
 	}
 	catch (e) {
 		console.log(e);
 		return message.channel.send(`command failed \`\`\`${e}\`\`\``);
 	}
-	message.channel.send(new Discord.MessageEmbed().setTitle('Success!').setDescription(`premium set to \`true\` in ${message.guild.name}! \n \n`).setFooter('thanks for being a donor :)').setColor('GREEN'));
+	message.channel.send(new Discord.MessageEmbed().setTitle('Success!').setDescription(`premium added to **${message.guild.name}**! \n \n`).setFooter('thanks for being a donor :)').setColor('GREEN'));
 
 };
 
 module.exports.help = {
-	aliases: ['psadd', 'premiumserveradd'],
-	name: 'premiumserver',
+	aliases: ['psadd', 'padd', 'premiumserver'],
+	name: 'premiumserveradd',
 	description: 'add premium to your server!',
 	usage: '.psadd',
 };
