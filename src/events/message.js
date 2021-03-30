2c70346b0757185ce994bee7d1fe957617518672
TS-7 <illuminati.aternos@gmail.com>
2021-03-30 22:44:00 +0530

    New permission checks for staff commands

--- src/events/message.js
+++ src/events/message.js
@@ -1,198 +1,308 @@
 //                                               -- All our requirements --
 
-const Discord = require('discord.js');
-const utils = require('../../utils/utils');
-const config = require('../../utils/config.json');
-const chatcord = require('chatcord');
+const Discord = require("discord.js");
+const utils = require("../../utils/utils");
+const config = require("../../utils/config.json");
+const chatcord = require("smartestchatbot");
+const staff = require("../../models/nuggiesStaff");
 const chatting = new chatcord.Client();
-const cmdhook = new Discord.WebhookClient(config.cmdhookID, config.cmdhookTOKEN);
-const errhook = new Discord.WebhookClient(config.errhookID, config.errhookTOKEN);
+const cmdhook = new Discord.WebhookClient(
+  config.cmdhookID,
+  config.cmdhookTOKEN
+);
+const errhook = new Discord.WebhookClient(
+  config.errhookID,
+  config.errhookTOKEN
+);
 
 module.exports = async (client, message) => {
-	//                                               -- Message Event Function --
-	if (!message.guild) return;
+  //                                               -- Message Event Function --
+  if (!message.guild) return;
 
-	// nuggies x pwetzel chat
+  // nuggies x pwetzel chat
 
-	if (message.channel.id === '814411982702510111' && message.author.id == '723112579584491571') {
-		setTimeout(async function() {
-			message.channel.startTyping();
-			await chatting.chat(message.content)
-				.then((reply) => {
-					message.inlineReply(Discord.Util.removeMentions(reply));
-					message.channel.stopTyping();
-				}).catch((error) => {
-					message.channel.send(`\`❌ CHAT ERROR\` \`\`\`xl\n${error}\n\`\`\``);
-					message.channel.stopTyping();
-				});
-		}, 3000);
-	}
+  if (
+    message.channel.id === "814411982702510111" &&
+    message.author.id == "723112579584491571"
+  ) {
+    setTimeout(async function () {
+      message.channel.startTyping();
+      await chatting
+        .chat({
+          message: encodeURIComponent(message.content),
+          name: "Nuggies",
+          owner: "AssassiN",
+          user: message.author.id,
+          language: "en",
+        })
+        .then((reply) => {
+          message.inlineReply(Discord.Util.removeMentions(reply));
+          message.channel.stopTyping();
+        })
+        .catch((error) => {
+          message.channel.send(`\`❌ CHAT ERROR\` \`\`\`xl\n${error}\n\`\`\``);
+          message.channel.stopTyping();
+        });
+    }, 3000);
+  }
 
-	if (message.author.bot) return;
+  if (message.author.bot) return;
 
-	// Fetch database
+  // Fetch database
 
-	const guildDB = await client.data.getGuildDB(message.guild.id);
-	const userDB = await client.data.getUserDB(message.author.id);
-	const data = {};
-	data.guild = guildDB;
-	data.user = userDB;
+  const guildDB = await client.data.getGuildDB(message.guild.id);
+  const userDB = await client.data.getUserDB(message.author.id);
+  const data = {};
+  data.guild = guildDB;
+  data.user = userDB;
 
-	// check if user is blacklisted
+  // check if user is blacklisted
 
-	if (data.user.blacklisted) return;
+  if (data.user.blacklisted) return;
 
-	// AFK thingy
+  // AFK thingy
 
-	if (userDB.is_afk) {
-		await client.data.removeAfk(message.author.id);
-		message.channel.send(Discord.Util.removeMentions('Welcome back **' + message.author.username + '**! You are no longer afk.'))
-		// eslint-disable-next-line no-unused-vars
-			.catch((error) => {
-				return true;
-			});
-	}
+  if (userDB.is_afk) {
+    await client.data.removeAfk(message.author.id);
+    message.channel
+      .send(
+        Discord.Util.removeMentions(
+          "Welcome back **" +
+            message.author.username +
+            "**! You are no longer afk."
+        )
+      )
+      // eslint-disable-next-line no-unused-vars
+      .catch((error) => {
+        return true;
+      });
+  }
 
-	message.mentions.users.forEach(async (u) => {
-		const userData = await client.data.getUserDB(u.id);
-		if (userData.is_afk) {
-			message.channel.send(`**${u.tag}** is currently afk for: **${userData.afkReason}**`)
-			// eslint-disable-next-line no-unused-vars
-				.catch((error) => {
-					return true;
-				});
-		}
-	});
+  message.mentions.users.forEach(async (u) => {
+    const userData = await client.data.getUserDB(u.id);
+    if (userData.is_afk) {
+      message.channel
+        .send(`**${u.tag}** is currently afk for: **${userData.afkReason}**`)
+        // eslint-disable-next-line no-unused-vars
+        .catch((error) => {
+          return true;
+        });
+    }
+  });
 
-	// Chatbot thingy
+  // Chatbot thingy
 
-	if (data.guild.chatbot_enabled && data.guild.chatbot_channel == message.channel.id) {
-		const badwords = ['nigger', 'nigga', 'nibba', 'nibber'];
-		const bl_log_channel = client.channels.cache.get('809317042058035241');
-		const reason = 'saying a blacklisted word.';
-		if (badwords.some((word) => message.content.toLowerCase().includes(word))) {
-			const blacklist = await client.data.blacklist(message.author.id, 'true', reason);
-			const logEmbed = new Discord.MessageEmbed().setTitle('<a:9689_tick:785181267758809120> User Blacklisted').setDescription(`**${message.author.username}#${message.author.discriminator}** was blacklisted from using the bot.\n\nResponsible Moderator : **${message.author.username}**\n\nReason : **${blacklist.reason}** \n **message**: ${message.content}`).setFooter('Blacklist registered').setColor('RED').setTimestamp();
-			bl_log_channel.send(logEmbed);
-			message.author.send(`You have been blacklisted from using the bot! \n **Reason:** ${reason}\n **Moderator:** ${message.author.tag} \n**Join Nuggies Support to appeal:** https://discord.gg/ut7PxgNdef`).catch((err) => {
-				message.channel.send(`${message.author.username} has DM's disabled. I was unable to send him a message - but blacklist has been registered!`);
-				console.log(err);
-				return;
-			});
-		}
+  if (
+    data.guild.chatbot_enabled &&
+    data.guild.chatbot_channel == message.channel.id
+  ) {
+    const badwords = ["nigger", "nigga", "nig", "nibba", "nibber"];
+    const bl_log_channel = client.channels.cache.get("809317042058035241");
+    const reason = "saying a blacklisted word.";
+    if (badwords.some((word) => message.content.toLowerCase().includes(word))) {
+      const blacklist = await client.data.blacklist(
+        message.author.id,
+        "true",
+        reason
+      );
+      const logEmbed = new Discord.MessageEmbed()
+        .setTitle("<a:9689_tick:785181267758809120> User Blacklisted")
+        .setDescription(
+          `**${message.author.username}#${message.author.discriminator}** was blacklisted from using the bot.\n\nResponsible Moderator : **${message.author.username}**\n\nReason : **${blacklist.reason}**`
+        )
+        .setFooter("Blacklist registered")
+        .setColor("RED")
+        .setTimestamp();
+      bl_log_channel.send(logEmbed);
+      message.author
+        .send(
+          `You have been blacklisted from using the bot! \n **Reason:** ${reason}\n **Moderator:** ${message.author.tag} \n**Join Nuggies Support to appeal:** https://discord.gg/ut7PxgNdef`
+        )
+        .catch((err) => {
+          message.channel.send(
+            `${message.author.username} has DM's disabled. I was unable to send him a message - but blacklist has been registered!`
+          );
+          console.log(err);
+          return;
+        });
+    }
 
-		const channel = data.guild.chatbot_channel;
-		if (!channel) return;
-		const sChannel = message.guild.channels.cache.get(channel);
-		if (message.author.bot || sChannel.id !== message.channel.id) return;
-		sChannel.startTyping();
+    const channel = data.guild.chatbot_channel;
+    if (!channel) return;
+    const sChannel = message.guild.channels.cache.get(channel);
+    if (message.author.bot || sChannel.id !== message.channel.id) return;
+    sChannel.startTyping();
 
-		if (!message.content) return;
+    if (!message.content) return;
 
-		await chatting.chat(message.content).then((reply) => {
-			return message.inlineReply(Discord.Util.removeMentions(reply));
-			// The module will reply with the based on stimulus (1st parameter of the chat function!)
-		}).catch((error) => {
-			return message.channel.send(
-				`\`❌ CHAT ERROR\` \`\`\`xl\n${error}\n\`\`\``,
-			);
-		});
-		sChannel.stopTyping();
-	}
+    await chatting
+      .chat({
+        message: message.content,
+        name: "Nuggies",
+        owner: "AssassiN",
+        user: message.author.id,
+        language: "en",
+      })
+      .then((reply) => {
+        return message.inlineReply(Discord.Util.removeMentions(reply));
+        // The module will reply with the based on stimulus (1st parameter of the chat function!)
+      })
+      .catch((error) => {
+        return message.channel.send(
+          `\`❌ CHAT ERROR\` \`\`\`xl\n${error}\n\`\`\``
+        );
+      });
+    sChannel.stopTyping();
+  }
 
-	if (message.author.bot) return;
+  if (message.author.bot) return;
 
-	// Ping Embed
-	// Get prefix from guild else get from config file
-	let prefixx = !guildDB.prefix ? config.prefix : guildDB.prefix;
-	if (client.user.id == '810385911459741786') {
-		prefixx = '..';
-	}
+  // Ping Embed
+  // Get prefix from guild else get from config file
+  const prefixx = !guildDB.prefix ? config.prefix : guildDB.prefix;
 
-	if (!message.author.bot && message.content.match(new RegExp(`^<@!?${client.user.id}>( |)$`))) {
-		const m = new Discord.MessageEmbed()
-			.setTitle('Hi, I\'m Nuggies !')
-			.setDescription('one of the most compact and easy to use bot on Discord !')
-			.addField('Prefix and Usage', 'My current prefixes are ' + `\`${prefixx}\` and <@${client.user.id}>` + `\n *Tip: Run \`${prefixx}help\` to get started! | use \`${prefixx}setprefix <prefix>\` to change prefix!*`)
-			.addField('Invites :', '[Support server](https://discord.gg/ut7PxgNdef) | [Bot invite](https://discord.com/oauth2/authorize?client_id=779741162465525790&permissions=1609952503&scope=bot%20applications.commands)')
-			.setColor('RANDOM');
-		message.channel.send(m);
-	}
-	// Basic command checks and argument definitions
+  if (
+    !message.author.bot &&
+    message.content.match(new RegExp(`^<@!?${client.user.id}>( |)$`))
+  ) {
+    const m = new Discord.MessageEmbed()
+      .setTitle("Hi, I'm Nuggies !")
+      .setDescription(
+        "one of the most compact and easy to use bot on Discord !"
+      )
+      .addField(
+        "Prefix and Usage",
+        "My current prefixes are " +
+          `\`${prefixx}\` and <@${client.user.id}>` +
+          "\n *Tip: Run `.help` to get started! | use `.setprefix <prefix>` to change prefix!*"
+      )
+      .addField(
+        "Invites :",
+        "[Support server](https://discord.gg/ut7PxgNdef) | [Bot invite](https://discord.com/oauth2/authorize?client_id=779741162465525790&permissions=1609952503&scope=bot%20applications.commands)"
+      )
+      .setColor("RANDOM");
+    message.channel.send(m);
+  }
+  // Basic command checks and argument definitions
 
-	const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
-	const prefix = !message.author.bot && message.content.match(prefixMention) ? !message.author.bot && message.content.match(prefixMention)[0] : prefixx;
+  const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
+  const prefix =
+    !message.author.bot && message.content.match(prefixMention)
+      ? !message.author.bot && message.content.match(prefixMention)[0]
+      : prefixx;
 
-	if (!message.content.startsWith(prefix)) return;
+  if (!message.content.startsWith(prefix)) return;
 
-	const args = message.content.slice(prefix.length).trim().split(/ +/g);
-	let command = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  let command = args.shift().toLowerCase();
 
-	// Command Handler Dynamic Checks
+  // Command Handler Dynamic Checks
 
-	if (client.aliases.has(command)) {
-		command = client.commands.get(client.aliases.get(command)).help.name;
-	}
+  if (client.aliases.has(command)) {
+    command = client.commands.get(client.aliases.get(command)).help.name;
+  }
 
-	const commandFile = client.commands.get(command);
+  const commandFile = client.commands.get(command);
 
-	if (!commandFile) return;
+  if (!commandFile) return;
 
-	if (client.commands.get(command).config.developers == true) {
-		if (!config.developers.includes(message.author.id)) {
-			return utils.errorEmbed(message, ':warning: This command is restricted only to bot owners.');
-		}
-	}
+  if (client.commands.get(command).config.developers == true) {
+    const nuggiesStaff = await staff.findOne({
+      _id: message.guild.id,
+    });
 
-	if (client.commands.get(command).config.restricted == true) {
-		if (!config.globalmods.includes(message.author.id) && !config.developers.includes(message.author.id)) {
-			return utils.errorEmbed(message, ':warning: This command is restricted only to bot moderators / owners.');
-		}
-	}
+    if (!nuggiesStaff.Developers.includes(message.author.id))
+      return utils.errorEmbed(
+        message,
+        ":warning: This command is restricted only to bot owners."
+      );
+  }
 
-	if (client.commands.get(command).config.disable == true) {
-		return utils.errorEmbed(message, ':warning: This command is disabled for a short period of time! :warning:');
-	}
+  if (client.commands.get(command).config.restricted == true) {
+    const nuggiesStaff = await staff.findOne({
+      _id: message.guild.id,
+    });
 
-	if (client.commands.get(command).config.args == true) {
-		if (!args[0]) {
-			return utils.errorEmbed(message, `Invalid arguments. Use: ${prefix + 'help ' + client.commands.get(command).help.name}`);
-		}
-	}
+    if (
+      !nuggiesStaff.Developers.includes(message.author.id) &&
+      !nuggiesStaff.Moderators.includes(message.author.id)
+    )
+      return utils.errorEmbed(
+        message,
+        ":warning: This command is restricted only to bot moderators / owners."
+      );
+  }
 
-	// Core Command Handler and Cooldown Checks
+  if (client.commands.get(command).config.disable == true) {
+    return utils.errorEmbed(
+      message,
+      ":warning: This command is disabled for a short period of time! :warning:"
+    );
+  }
 
-	const cooldown = client.commands.get(command).config.cooldown;
-	const timestamps = client.cooldowns.get(command);
-	if (timestamps.has(message.author.id)) {
-		const expirationTime = timestamps.get(message.author.id) + cooldown;
-		if (Date.now() < expirationTime) {
-			const timeLeft = utils.timer(expirationTime);
-			return message.channel.send(`**\`${message.author.username}\`** | ⏰ Hold up! Command in cooldown for **\`${timeLeft}\`**`);
-		}
-	}
+  if (client.commands.get(command).config.args == true) {
+    if (!args[0])
+      return utils.errorEmbed(
+        message,
+        `Invalid arguments. Use: ${
+          prefix + "help " + client.commands.get(command).help.name
+        }`
+      );
+  }
 
-	// Command Logs
+  // Core Command Handler and Cooldown Checks
 
-	if (commandFile) {
-		try {
-			if (client.user.id === '779741162465525790') {
-				if (!command) return;
-				const m = new Discord.MessageEmbed().setTitle(`Command used in ${message.guild.name}`).setColor('RANDOM').addField('User:', `${message.author.tag}`).addField('User ID:', `${message.author.id}`).addField('Command:', `${command}`).addField('Message Content:', `${message.content}`).addField('Guild ID:', `${message.guild.id}`);
-				await cmdhook.send(m);
-			}
-			await timestamps.set(message.author.id, Date.now());
-			setTimeout(
-				async () => await timestamps.delete(message.author.id), cooldown);
-			await commandFile.run(client, message, args, utils, data);
-		}
-		catch (error) {
-			// Command Errors
-			if (client.user.id === '779741162465525790') {
-				const errEmbed = new Discord.MessageEmbed().setTitle(`Command error in ${message.guild.name}`).addField('Additional Details', `**Guild ID :** ${message.guild.id}\n**Author :** ${message.author.tag}(${message.author.id})\n**Command :** ${commandFile.help.name}\n**Content :** ${message.content}`, false).setDescription(`**Error:**\n\`\`\`js\n${error}\n\`\`\``).setTimestamp();
-				errhook.send(errEmbed);
-			}
-			return message.channel.send(`\`❌ COMMAND ERROR\` \`\`\`xl\n${error.message}\n\`\`\``);
-		}
-	}
+  const cooldown = client.commands.get(command).config.cooldown;
+  const timestamps = client.cooldowns.get(command);
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldown;
+    if (Date.now() < expirationTime) {
+      const timeLeft = utils.timer(expirationTime);
+      return message.channel.send(
+        `**\`${message.author.username}\`** | ⏰ Hold up! Command in cooldown for **\`${timeLeft}\`**`
+      );
+    }
+  }
+
+  // Command Logs
+
+  if (commandFile) {
+    try {
+      if (client.user.id === "779741162465525790") {
+        if (!command) return;
+        const m = new Discord.MessageEmbed()
+          .setTitle(`Command used in ${message.guild.name}`)
+          .setColor("RANDOM")
+          .addField("User:", `${message.author.tag}`)
+          .addField("User ID:", `${message.author.id}`)
+          .addField("Command:", `${command}`)
+          .addField("Message Content:", `${message.content}`)
+          .addField("Guild ID:", `${message.guild.id}`);
+        await cmdhook.send(m);
+      }
+      await timestamps.set(message.author.id, Date.now());
+      setTimeout(
+        async () => await timestamps.delete(message.author.id),
+        cooldown
+      );
+      await commandFile.run(client, message, args, utils, data);
+    } catch (error) {
+      // Command Errors
+      if (client.user.id === "779741162465525790") {
+        const errEmbed = new Discord.MessageEmbed()
+          .setTitle(`Command error in ${message.guild.name}`)
+          .addField(
+            "Additional Details",
+            `**Guild ID :** ${message.guild.id}\n**Author :** ${message.author.tag}(${message.author.id})\n**Command :** ${commandFile.help.name}\n**Content :** ${message.content}`,
+            false
+          )
+          .setDescription(`**Error:**\n\`\`\`js\n${error}\n\`\`\``)
+          .setTimestamp();
+        errhook.send(errEmbed);
+      }
+      return message.channel.send(
+        `\`❌ COMMAND ERROR\` \`\`\`xl\n${error.message}\n\`\`\``
+      );
+    }
+  }
 };
