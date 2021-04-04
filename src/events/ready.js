c45caddbc6d38015cd9d5f5893d26103abbd523a
pro_gamer007 <coolaryan@icloud.com>
2021-04-04 15:59:22 +0530

    how bout we do this :trollface:

--- src/events/ready.js
+++ src/events/ready.js
@@ -6,13 +6,15 @@ const config = require('../../utils/config.json');
 module.exports = async (client) => {
 	console.log(`${client.user.username} is now online!`);
 	client.user.setActivity(`to ${client.guilds.cache.size} servers!`, { type: 'STREAMING', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' });
-	const Webhook = new Discord.WebhookClient(config.restarthookID, config.restarthookTOKEN);
-	const restartEmbed = new Discord.MessageEmbed()
-		.setTitle('Nuggies was restarted!')
-		.setDescription('Nuggies just got restarted!')
-		.setColor('e03854')
-		.setFooter(client.user.id)
-		.setTimestamp();
-	Webhook.send(restartEmbed);
+	if (!client.user.id == '779741162465525790') {
+		const Webhook = new Discord.WebhookClient(config.restarthookID, config.restarthookTOKEN);
+		const restartEmbed = new Discord.MessageEmbed()
+			.setTitle('Nuggies was restarted!')
+			.setDescription('Nuggies just got restarted!')
+			.setColor('e03854')
+			.setFooter(client.user.id)
+			.setTimestamp();
+		Webhook.send(restartEmbed);
+	}
 	// await automeme.updateMany({ automeme_enabled: true }, { premium: false });
 };
