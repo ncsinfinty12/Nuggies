2444a9fb8af86230669a9f5e8cd7035dddc33418
Assassin-1234 <chordsandfrets@gmail.com>
2021-04-06 14:38:39 +0530

    automeme reverted

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Discord = require('discord.js');
 const automeme = require('../../models/guilds');
 const config = require('../../utils/config.json');
+const axios = require('axios');
 module.exports = async (client) => {
 	console.log(`${client.user.username} is now online!`);
 	client.user.setActivity(`to ${client.guilds.cache.size} servers!`, { type: 'STREAMING', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' });
@@ -13,5 +14,22 @@ module.exports = async (client) => {
 		.setColor('e03854')
 		.setTimestamp();
 	Webhook.send(restartEmbed);
-	// await automeme.updateMany({ automeme_enabled: true }, { premium: false });
+
+	// automeme
+	automeme.find({ automeme_enabled: true }, async (err, data) => {
+		setInterval(() => {
+			for (let i = 0; i < data.length; i++) {
+				axios.get('https://api.nuggies.tech/api/meme')
+					.then(function(response) {
+						const embed = new Discord.MessageEmbed()
+							.setTitle(`${response.data.title}`)
+							.setURL(`${response.data.url}`)
+							.setImage(response.data.image)
+							.setColor('RANDOM')
+							.setFooter(`👍 ${response.data.upvotes} 👎 ${response.data.downvotes} 💬 ${response.data.comments}`);
+						client.channels.cache.get(data[i].automeme_channel).send(embed);
+					});
+			}
+		}, 10000);
+	});
 };
