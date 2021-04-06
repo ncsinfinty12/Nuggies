a9018bb8379649190344c0d61d08d8b93125cf15
Assassin-1234 <chordsandfrets@gmail.com>
2021-04-06 22:00:06 +0530

    fixed ayo

--- src/commands/owner/ayo.js
+++ src/commands/owner/ayo.js
@@ -2,17 +2,17 @@
 const Discord = require('discord.js');
 const { getUserDB } = require('../../../functions/mongo');
 module.exports.run = async (client, message, args, utils, data) => {
-	const target = message.mentions.members.first() || message.guild.members.cache.find((m) => m.user.id === args[0] || m.user.tag.startsWith(args[0]) || m.displayName.startsWith(args[0]));
+	const target = args[0];
 	if(!target) return message.channel.send('please provide a user id to ayo.');
-	client.users.fetch(target.id);
-	const gettier = await getUserDB(target.id);
+	client.users.fetch(args[0]);
+	const gettier = await getUserDB(target);
 	let tier = 0;
 	if(gettier.tier == 1) tier = 3;
 	if(gettier.tier == 2) tier = 5;
 	if(gettier.tier == 3) tier = 5;
 	if(gettier.tier == 4) tier = 3;
 	if(gettier.tier == 5) tier = 'unlimited';
-	target.send(new Discord.MessageEmbed().setTitle('Thank you for Supporting us!').setColor('GREEN').setDescription(`I have added **premium tier ${gettier.tier}** to you! You can add **Nuggies Premium to ${tier} servers!** \n \n Join [this server](https://discord.gg/d98jT3mgxf) to get a special role!`).setFooter('you also get automeme, big pp in .pp command less cooldowns!').setThumbnail(target.user.displayAvatarURL({ dynamic:true })));
+	client.users.cache.get(target).send(new Discord.MessageEmbed().setTitle('Thank you for Supporting us!').setColor('GREEN').setDescription(`I have added **premium tier ${gettier.tier}** to you! You can add **Nuggies Premium to ${tier} servers!** \n \n Join [this server](https://discord.gg/d98jT3mgxf) to get a special role!`).setFooter('you also get automeme, big pp in .pp command less cooldowns!').setThumbnail(client.users.cache.get(target).displayAvatarURL({ dynamic:true })));
 	message.channel.send('successfully ayo\'d that person');
 };
 
