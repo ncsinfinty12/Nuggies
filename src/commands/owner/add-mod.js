1c64c25126276cde59acece7d51e594c586dd7f9
Assassin-1234 <chordsandfrets@gmail.com>
2021-04-06 14:27:46 +0530

    Update add-mod.js

--- src/commands/owner/add-mod.js
+++ src/commands/owner/add-mod.js
@@ -1,75 +1,69 @@
 /* eslint-disable no-unused-vars */
-const Discord = require("discord.js");
-const config = require("../../../utils/config.json");
-const user = require("../../../models/users");
+const Discord = require('discord.js');
+const config = require('../../../utils/config.json');
+const user = require('../../../models/users');
 
 module.exports.run = async (client, message, args) => {
-	const target =
-    message.mentions.members.first() ||
-    message.guild.members.cache.find(
-      (m) =>
-        m.user.id === args[0] ||
-        m.user.tag.startsWith(args[0]) ||
-        m.displayName.startsWith(args[0])
-    );
+	const target = message.mentions.members.first() || message.guild.members.cache.find((m) => m.user.id === args[0] || m.user.tag.startsWith(args[0]) || m.displayName.startsWith(args[0]));
 
-    const superMods = ["555064829946232832", "734006373343297557", "460078206326800434"]
-    if(!superMods.includes(message.author.id)) {
-      return message.channel.send(
-        new Discord.MessageEmbed()
-          .setTitle(`:warning: Failed`)
-          .setDescription(
-            `You do not have enough privileges to execute this command`
-          )
-          .setColor(`RED`)
-      );
-    }
+	const superMods = ['555064829946232832', '734006373343297557', '460078206326800434'];
+	if(!superMods.includes(message.author.id)) {
+		return message.channel.send(
+			new Discord.MessageEmbed()
+				.setTitle(':warning: Failed')
+				.setDescription(
+					'You do not have enough privileges to execute this command',
+				)
+				.setColor('RED'),
+		);
+	}
 
-    if(!target) return message.channel.send(`You did not mention a user to grant privileges to.`)
-    
-    const checkTarget = message.guild.members.cache.get(`${target.id}`);
-    if (!checkTarget) {
-      return message.channel.send(
-        new Discord.MessageEmbed()
-          .setTitle(`:warning: Error`)
-          .setDescription(
-            `${target} is not a valid user. Not even sure what you're on about`
-          )
-          .setColor(`RED`)
-      );
-    }
+	if(!target) return message.channel.send('You did not mention a user to grant privileges to.');
 
-  const fetch = await client.data.getUserDB(target.id);
+	const checkTarget = message.guild.members.cache.get(`${target.id}`);
+	if (!checkTarget) {
+		return message.channel.send(
+			new Discord.MessageEmbed()
+				.setTitle(':warning: Error')
+				.setDescription(
+					`${target} is not a valid user. Not even sure what you're on about`,
+				)
+				.setColor('RED'),
+		);
+	}
 
-    if (fetch.moderator) {
-      return message.channel.send(
-        new Discord.MessageEmbed()
-          .setTitle(`:warning: Error`)
-          .setDescription(`This user already has moderator permissions`)
-          .setColor(`RED`)
-      );
-    } else {
-      await client.data.moderator(target.id, 'true')
+	const fetch = await client.data.getUserDB(target.id);
 
-      message.channel.send(
-        new Discord.MessageEmbed()
-          .setTitle(`<a:9689_tick:785181267758809120> Success!`)
-          .setDescription(
-            `**<@${target.id}>** was successfully given moderator permissions`
-          )
-          .setColor(`GREEN`)
-      );
-    return message.guild.members.cache
-      .get(target.id)
-      .send(
-        new Discord.MessageEmbed()
-          .setTitle(`:warning: Alert`)
-          .setDescription(
-            `You were granted moderator permissions by **${message.author.tag}** for **Nuggies**`
-          )
-          .setColor(`GREEN`)
-      );
-  }
+	if (fetch.moderator) {
+		return message.channel.send(
+			new Discord.MessageEmbed()
+				.setTitle(':warning: Error')
+				.setDescription('This user already has moderator permissions')
+				.setColor('RED'),
+		);
+	}
+	else {
+		await client.data.moderator(target.id, 'true');
+
+		message.channel.send(
+			new Discord.MessageEmbed()
+				.setTitle('<a:9689_tick:785181267758809120> Success!')
+				.setDescription(
+					`**<@${target.id}>** was successfully given moderator permissions`,
+				)
+				.setColor('GREEN'),
+		);
+		return message.guild.members.cache
+			.get(target.id)
+			.send(
+				new Discord.MessageEmbed()
+					.setTitle(':warning: Alert')
+					.setDescription(
+						`You were granted moderator permissions by **${message.author.tag}** for **Nuggies**`,
+					)
+					.setColor('GREEN'),
+			);
+	}
 };
 
 module.exports.help = {
@@ -80,7 +74,7 @@ module.exports.help = {
 };
 
 module.exports.config = {
-  category: "Owner",
-  disable: false,
-  cooldown: 2000,
+	category: 'Owner',
+	disable: false,
+	cooldown: 2000,
 };
