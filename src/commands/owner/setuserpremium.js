099e8c05b6c24e1980475da371da8a399ec11725
Assassin-1234 <chordsandfrets@gmail.com>
2021-04-06 21:59:05 +0530

    damn fixed userpremium

--- src/commands/owner/setuserpremium.js
+++ src/commands/owner/setuserpremium.js
@@ -2,19 +2,18 @@
 const Discord = require('discord.js');
 const users = require('../../../models/users');
 module.exports.run = async (client, message, args, utils, data) => {
-	const target = message.mentions.members.first() || message.guild.members.cache.find((m) => m.user.id === args[0] || m.user.tag.startsWith(args[0]) || m.displayName.startsWith(args[0]));
-	if(!target) return message.channel.send('bruh fartpoop poop specify a user');
-	const tier = args[1];
-	await client.users.fetch(target.id);
+
+	const target = args[0];
 	if(!target) return message.channel.send(new Discord.MessageEmbed().setTitle('Error').setDescription('Please provide a user!').setColor('RED'));
-	// if(toggle != 'true' || 'false') return message.channel.send(new Discord.MessageEmbed().setTitle('Error').setDescription('Please provide a valid toggle!'));
-	users.findOne({ id: target.id }, async (err, Data) => {
+	const tier = args[1];
+	await client.users.fetch(target);
+	users.findOne({ id: target }, async (err, Data) => {
 		if(err) throw err;
 		if(!Data) return message.channel.send(new Discord.MessageEmbed().setTitle('Error').setDescription('user not found').setColor('RED'));
 		Data.tier = tier;
 		Data.save();
-		message.channel.send(new Discord.MessageEmbed().setTitle('Success!').setDescription(`premium set to \`tier ${tier}\` for **${client.users.cache.get(target.id).tag}** `).setColor('GREEN'));
-		client.channels.cache.get('828996803855777882').send(new Discord.MessageEmbed().setTitle(`Premium tier ${tier} added to ${target.user.username}`).setColor('GREEN'));
+		message.channel.send(new Discord.MessageEmbed().setTitle('Success!').setDescription(`premium set to \`tier ${tier}\` for **${client.users.cache.get(target).tag}** `).setColor('GREEN'));
+		client.channels.cache.get('828996803855777882').send(new Discord.MessageEmbed().setTitle(`Premium tier ${tier} added to ${client.users.cache.get(target).username}`).setColor('GREEN'));
 	});
 };
 module.exports.help = {
