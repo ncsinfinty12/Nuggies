a30b9956bae171ee6f87b33a2edee4dc629e544d
Assassin-1234 <chordsandfrets@gmail.com>
2021-04-08 00:00:51 +0530

    ExtendedGiveawayManager.js created

---
+++ utils/ExtendedGiveawayManager.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+const { Client } = require('discord.js');
+const { Database } = require('quickmongo');
+const { GiveawaysManager } = require('discord-giveaways');
+const client = new Client();
+const db = new Database('mongodb+srv://Assassin1234:K@rt00$99@cluster0.qonl3.mongodb.net/Nuggies_main');
+class extendedGiveawaysManager extends GiveawaysManager {
+	async refreshStorage() {
+		return client.shard.broadcastEval(() => this.giveawaysManager.getAllGiveaways());
+	}
+
+	async getAllGiveaways() {
+		return await db.get('giveaways');
+	}
+
+	async saveGiveaway(messageID, giveawayData) {
+		await db.push('giveaways', giveawayData);
+		return true;
+	}
+
+	async editGiveaway(messageID, giveawayData) {
+		const giveaways = await db.get('giveaways');
+		const newGiveawaysArray = giveaways.filter((giveaway) => giveaway.messageID !== messageID);
+		newGiveawaysArray.push(giveawayData);
+		await db.set('giveaways', newGiveawaysArray);
+		return true;
+	}
+
+	async deleteGiveaway(messageID) {
+		const data = await db.get('giveaways');
+		const newGiveawaysArray = data.filter((giveaway) => giveaway.messageID !== messageID);
+		await db.set('giveaways', newGiveawaysArray);
+		return true;
+	}
+}
